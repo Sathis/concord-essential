bc8a76b3a7126092a0117c326909d80273875bb0
Sathish Gopal(UST <U20931@ust-global.com>
2021-02-26 00:51:43 +0530

    Reading json file

--- test.groovy
+++ test.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonSlurper
 @Grab(group='org.apache.httpcomponents.client5', module='httpclient5', version='5.0.3')
 
 import org.apache.hc.client5.http.classic.methods.HttpGet
@@ -5,10 +6,16 @@ import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
 
 
 def httpClient = HttpClientBuilder.create().build()
+
+def jsonSlurper = new JsonSlurper()
+def data = jsonSlurper.parse(new File("demo.json"))
+
+println(data.name)
+
 println("Invoking http call")
+
 httpClient.execute(new HttpGet("http://google.com"))
 .withCloseable {response ->
     println(response.getCode())
 
 }
-
